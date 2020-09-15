1b851d3bd08ff9130e6f18cb703fea280407ecd1
hcolina <slushball420@gmail.com>
2020-09-14 21:53:19 -0500

    commit

---
+++ AnimatedText/AnimatedText/AnimatedText/Models/Budget.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AnimatedText.Models
+{
+    public class Budget
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public float Amount { get; set; }
+        public Color Color { get; set; }
+    }
+}
